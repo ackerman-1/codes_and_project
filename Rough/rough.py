afcf667b8ef6299a613f858a261729e35b002b73
ackerman-1 <parthktiwari@gmail.com>
2023-04-06 15:22:13 +0530

    SHA 256

--- Rough/rough.py
+++ Rough/rough.py
@@ -1,7 +1,7 @@
 import hashlib
 message1 = "The flag is 23F-Hj!-9989".encode()
 message2 = "The flax is 23F-Hj!-9989".encode()
-# message2 = "Titan".encode()
+
 hash1 = hashlib.sha256(message1).hexdigest()
 hash2 = hashlib.sha256(message2).hexdigest()
 print(hash1)
